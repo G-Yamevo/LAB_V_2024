ed3174d01fb518ed2f80247a95ca8a6bb79f22d0
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2024-03-31 14:00:46 -0300

    Creacion de las clases principal y Teatro

---
+++ TP1_L5_GRUPO_6_1/src/dominio/Principal.java
@@ -0,0 +1,12 @@
+package dominio;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Entrada ent = new Entrada();
+  System.out.println(ent.getCosto());
+  
+	}
+
+}
