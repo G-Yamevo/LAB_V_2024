2ccff8c6a09d3bc05a1add82ce91e6e2e88ccc32
Luketruke <lucaszabalett@gmail.com>
2024-04-01 15:26:09 -0300

    Cambio main

--- TP1_L5_GRUPO_6_1/src/dominio/Principal.java
+++ TP1_L5_GRUPO_6_1/src/dominio/Principal.java
@@ -4,9 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  Entrada ent = new Entrada();
-  System.out.println(ent.getCosto());
-  
+		Recital recital = new Recital(123, "Nombre del Recital", "Lunes", "20:00", "2 horas", 800, "Banda 1", 
+				"Rock", "Banda Soporte 1", "Banda Soporte 2", false);
+		System.out.println(recital.getCosto());
+
 	}
 
 }
