ecbb28d29a790f807f4bc65dfcdcaeba26552336
Santiago Ross <santidaross@outlook.com>
2024-04-02 02:17:17 -0300

    Se agrega test en main.

--- TP1_L5_GRUPO_6_1/src/dominio/Principal.java
+++ TP1_L5_GRUPO_6_1/src/dominio/Principal.java
@@ -4,8 +4,14 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Recital recital = new Recital(123, "Nombre del Recital", "Lunes", "20:00", "2 horas", 800, "Banda 1", 
-				"Rock", "Banda Soporte 1", "Banda Soporte 2", false);
+		Recital recital = new Recital(123, "Nombre del Recital", "Lunes", "20:00", "2 horas", 800, "Banda 1", "Rock",
+				"Banda Soporte 1", "Banda Soporte 2", false);
 		System.out.println(recital.getCosto());
+		
+		// Test de la clase EntradaDeporte
+		EntradaDeporte entradaDeporte = new EntradaDeporte(1, "Test futbol", "1", "23:39", "3 horas",  false,
+				"Futbol");
+		System.out.println(entradaDeporte.toString());
 	}
+	
 }
