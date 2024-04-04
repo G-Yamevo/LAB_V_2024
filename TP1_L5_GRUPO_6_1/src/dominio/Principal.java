c7b16a0dfcffeda55342c7aba591b69fbefd8f34
Santiago Ross <santidaross@outlook.com>
2024-04-03 21:51:19 -0300

    Agregadas nuevas bandas de soporte

--- TP1_L5_GRUPO_6_1/src/dominio/Principal.java
+++ TP1_L5_GRUPO_6_1/src/dominio/Principal.java
@@ -8,7 +8,6 @@ import interfaces.ImprimibleEnConsola;
 public class Principal {
 
 	public static void main(String[] args) {
-		
 		List<Genero> generosRecitales = new ArrayList<>();
 		generosRecitales.add(new Genero("Rock"));
 		generosRecitales.add(new Genero("Heavy Metal"));
@@ -16,27 +15,22 @@ public class Principal {
 		generosRecitales.add(new Genero("Trap"));
 		generosRecitales.add(new Genero("Latinos"));
 		generosRecitales.add(new Genero("Pop"));
-		
 		List<Genero> generosTeatro = new ArrayList<>();
 		generosTeatro.add(new Genero("Drama"));
 		generosTeatro.add(new Genero("Teatro"));
 		generosTeatro.add(new Genero("Comedia"));
-		
 		List<Deporte> deportes = new ArrayList<>();
 		deportes.add(new Deporte("Futbol"));
 		deportes.add(new Deporte("Hockey"));
 		deportes.add(new Deporte("Rugby"));
-		
 		List<BandaRecitales> bandas = new ArrayList<>();
 		bandas.add(new BandaRecitales("Los Piojos"));
 		bandas.add(new BandaRecitales("Miranda"));
-		
 		List<BandaRecitales> bandasSoporte = new ArrayList<>();
-		bandasSoporte.add(new BandaRecitales("Los Piojos"));
-		bandasSoporte.add(new BandaRecitales("Miranda"));
-		bandasSoporte.add(new BandaRecitales("Los Piojos"));
-		bandasSoporte.add(new BandaRecitales("Miranda"));
-		
+		bandasSoporte.add(new BandaRecitales("Vox Dei"));
+		bandasSoporte.add(new BandaRecitales("León Gieco"));
+		bandasSoporte.add(new BandaRecitales("Soda Stereo"));
+		bandasSoporte.add(new BandaRecitales("Los Fabulosos Cadillacs"));
 		List<ImprimibleEnConsola> entradas = new ArrayList<>();
 		entradas.add(new EntradaRecital("Naranja Persa", "Lunes", "20:00", "2 horas", bandas.get(0),
 				generosRecitales.get(0), bandasSoporte.get(0), bandasSoporte.get(1), false));
@@ -51,7 +45,6 @@ public class Principal {
 		entradas.add(new EntradaInfantil("Piñon Fijo", "Domingo", "18:00 hrs", "3 hs", false, false));
 		entradas.add(new EntradaTeatro("FANTASMA DE LA OPERA", "Sabado", "22:00 hrs", "3 hs", generosTeatro.get(1),
 				"Ricardo Darin", "Guillermo Francella", "Erica Rivas"));
-		
 		for (ImprimibleEnConsola entrada : entradas) {
 			entrada.imprimirEnConsola();
 		}
